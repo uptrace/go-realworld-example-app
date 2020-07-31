df4690cdd46357c8d9b25f5eebfe9444dab6ecef
Anatolii Mihailenco <anmic.webdev@gmail.com>
2020-07-31 17:06:05 +0300

    WIP

--- org/user_api.go
+++ org/user_api.go
@@ -6,50 +6,51 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-func listUsers(c *gin.Context) error {
+func listUsers(c *gin.Context) {
 	var users []User
 	if err := rwe.PGMain().
 		Model(&users).
 		Select(); err != nil {
-		return err
+		c.Error(err)
+		return
 	}
 
 	c.JSON(200, gin.H{"users": users})
-	return nil
 }
 
-func showUser(c *gin.Context) error {
+func showUser(c *gin.Context) {
 	user := new(User)
 	if err := rwe.PGMain().
 		Model(user).
 		Where("user_id = ?", c.Param("user_id")).
 		Select(); err != nil {
-		return err
+		c.Error(err)
+		return
 	}
 
 	c.JSON(200, gin.H{"users": user})
-	return nil
 }
 
-func createUser(c *gin.Context) error {
+func createUser(c *gin.Context) {
 	user := new(User)
 	c.BindJSON(user)
 
 	var err error
 	user.PasswordHash, err = hashPassword(user.PasswordHash)
 	if err != nil {
-		return err
+		c.Error(err)
+		return
 	}
 
 	_, err = rwe.PGMain().
 		Model(user).
 		Insert()
 	if err != nil {
-		return err
+		c.Error(err)
+		return
 	}
 
 	c.JSON(200, gin.H{"user": user})
-	return nil
 }
 
 func hashPassword(pass string) (string, error) {
