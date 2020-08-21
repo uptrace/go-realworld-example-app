1729d49e572a7b6df59b3760c42d4c19db9db0c1
Anatolii Mihailenco <anmic.webdev@gmail.com>
2020-08-21 18:30:28 +0300

    WIP

--- rwe/router.go
+++ rwe/router.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	ginInstr "go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin"
+	gintrace "go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin"
 )
 
 var (
@@ -16,7 +16,7 @@ func init() {
 	Router = gin.Default()
 	Router.Use(corsMiddleware)
 	Router.Use(errorMiddleware)
-	Router.Use(ginInstr.Middleware("rwe"))
+	Router.Use(gintrace.Middleware("rwe"))
 
 	API = Router.Group("/api")
 }
